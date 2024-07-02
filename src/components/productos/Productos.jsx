12efdd25e3a91c7e0b2aa034df4db4d01f795ce3
Bruxar <larancibia@utem.cl>
2024-07-02 18:45:00 -0400

    first commit

---
+++ src/components/productos/Productos.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Productos(params) {
+    return (
+        <h2>Productos</h2>
+    );
+}
+
+export default Productos;
---
+++ src/utils/fontawesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser, faBox, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faUser, faBox, faShoppingCart);
